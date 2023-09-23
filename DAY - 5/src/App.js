ce0c6b6449263414d985cb346fc48b7c12a12463
yuven2511 <120372522+yuven2511@users.noreply.github.com>
2023-09-23 09:47:08 +0530

    Add files via upload

---
+++ DAY - 5/src/App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import './assets/css/App.css';
+import DashboardAdmin from './components/Admin/Dashboard/DashboardAdmin.jsx';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <DashboardAdmin/>
+
+    </div>
+  );
+}
+
+export default App;
