e5406d3bbfbf9c1fca7c177fea8fb5317a500a21
yuven2511 <120372522+yuven2511@users.noreply.github.com>
2023-10-09 11:50:01 +0530

    Add files via upload

---
+++ Day -13/src/main/java/com/mindunits/yuven/dto/response/UserResponse.java
@@ -0,0 +1,20 @@
+package com.mindunits.yuven.dto.response;
+
+import com.mindunits.yuven.model.enumerate.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long uid;
+    private String name;
+    private String email;
+    private Boolean isEnabled;
+    private Role role;
+}
---
+++ Day -13/src/main/java/com/mindunits/yuven/model/enumerate/Role.java
@@ -0,0 +1,9 @@
+package com.mindunits.yuven.model.enumerate;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
